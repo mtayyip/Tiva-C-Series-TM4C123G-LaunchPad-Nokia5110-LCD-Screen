53a95cb55a4080c1fa8ff10219584d7f69ec750a
Muhammet Tayyip Çankaya <mycankaya@gtu.edu.tr>
2018-10-28 17:15:59 +0300

    nokia 5110 lcd screen

---
+++ 5110Lcd/5510lcd/Debug/main.d
@@ -0,0 +1,15 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.7.LTS/include/stdint.h
+main.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.7.LTS/include/stdbool.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/sysctl.h
+main.obj: ../Nokia5110.h
+main.obj: ../bitmapPhotos.h
+
+../main.c: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.7.LTS/include/stdint.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.7.LTS/include/stdbool.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/sysctl.h: 
+../Nokia5110.h: 
+../bitmapPhotos.h: 
